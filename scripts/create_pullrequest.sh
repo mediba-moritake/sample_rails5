a1cfc63f6cc57209ccb92af7b36aad5d98498d14
Yasufumi Moritake <moritake@mediba.jp>
2016-09-02 20:13:21 +0900

    :+1: Update git

--- scripts/create_pullrequest.sh
+++ scripts/create_pullrequest.sh
@@ -3,11 +3,11 @@ set -e
 
 export BRANCH=bundle_update_$(date -u "+%Y%m%d")
 
-git config --global user.email travisci@mediba.jp
+git config --global user.email 'travisci@mediba.jp'
 git config --global user.name 'TravisCI'
+git checkout -b $BRANCH
 git add Gemfile.lock
 git commit -m '$ bundle update'
-git branch -M $BRANCH
 git push origin $BRANCH
 
 bundle exec ruby scripts/create_pullrequest.rb
