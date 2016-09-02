b7e088920540be6b9cdcde504fd29f57efe106ed
Yasufumi Moritake <moritake@mediba.jp>
2016-09-02 22:06:01 +0900

    :+1: Update git

--- scripts/create_pullrequest.sh
+++ scripts/create_pullrequest.sh
@@ -5,9 +5,10 @@ export BRANCH=bundle_update_$(date -u "+%Y%m%d")
 
 git config --global user.email 'travisci@mediba.jp'
 git config --global user.name 'TravisCI'
+git init
 git checkout -b $BRANCH
 git add Gemfile.lock
 git commit -m '$ bundle update'
-git push origin $BRANCH
+git push --force --quiet "https://${GITHUB_ACCESS_TOKEN}@github.com/mediba-moritake/sample_rails5.git" $BRANCH:$BRANCH > /dev/null 2>&1
 
 bundle exec ruby scripts/create_pullrequest.rb
