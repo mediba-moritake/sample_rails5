863bcf54c7aec79e66385ebe777b4b7d496dccb1
Yasufumi Moritake <moritake@mediba.jp>
2016-09-08 18:12:24 +0900

    :+1: Test

--- scripts/travisci-bundle-update-pr.sh
+++ scripts/travisci-bundle-update-pr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
+if [ "${TRAVIS_EVENT_TYPE}" = "push" ]; then
     gem install travisci-bundle-update-pr
     travisci-bundle-update-pr TravisCI travisci@travisci-bundle-update-pr.com
 fi
