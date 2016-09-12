1cc0b1780b1ab1dbd0b956d3ccd47a78ce4c11be
Yasufumi Moritake <moritake@mediba.jp>
2016-09-12 15:04:47 +0900

    :+1: Update gem

--- sample_project/scripts/travisci-bundle-update-pr.sh
+++ sample_project/scripts/travisci-bundle-update-pr.sh
@@ -3,5 +3,5 @@ set -ev
 
 if [ "${TRAVIS_EVENT_TYPE}" = "push" ]; then
     gem install $TRAVIS_BUILD_DIR/pkg/travisci-bundle-update-pr-0.0.10.gem
-    travisci-bundle-update-pr TravisCI travisci@travisci-bundle-update-pr.com sample_project
+    travisci-bundle-update-pr TravisCI travisci@travisci-bundle-update-pr.com master sample_project
 fi
